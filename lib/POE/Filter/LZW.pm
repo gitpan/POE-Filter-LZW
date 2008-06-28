d9cd9086a3a1096175222c5d9283eb72529efdb3
Chris Williams <chris@bingosnet.co.uk>
2008-06-28 08:28:52 +0000

    Import of BINGOS/POE-Filter-LZW-1.68 from CPAN.
    gitpan-cpan-distribution: POE-Filter-LZW
    gitpan-cpan-version:      1.68
    gitpan-cpan-path:         BINGOS/POE-Filter-LZW-1.68.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Filter/LZW.pm
+++ lib/POE/Filter/LZW.pm
@@ -7,7 +7,7 @@ use Compress::LZW qw(compress decompress);
 use vars qw($VERSION);
 use base qw(POE::Filter);
 
-$VERSION = '1.66';
+$VERSION = '1.68';
 
 sub new {
   my $type = shift;
@@ -150,6 +150,12 @@ Sets the compression level. Consult L<Compress::LZW> for details.
 =head1 AUTHOR
 
 Chris Williams <chris@bingosnet.co.uk>
+
+=head1 LICENSE
+
+Copyright (C) Chris Williams
+
+This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
 
 =head1 SEE ALSO
 
